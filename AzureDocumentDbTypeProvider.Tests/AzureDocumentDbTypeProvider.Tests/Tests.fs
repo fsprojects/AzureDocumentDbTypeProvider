decb9f2202e70cd44fa145d634f70c3cef982c7c
Stewart Robertson <stewart_robertson@sky.com>
2016-07-13 13:13:09 +0100

    Added tests to build script

---
+++ AzureDocumentDbTypeProvider.Tests/AzureDocumentDbTypeProvider.Tests/Tests.fs
@@ -0,0 +1,10 @@
+﻿module Tests
+
+open Xunit
+
+[<Fact>]
+let ``Can Run Tests``() = Assert.True(true)
+
+// [<Fact>]
+// let ``Can List all dbs``() = 
+//     Assert.True(false)
--- build.fsx
+++ build.fsx
@@ -1,22 +1,35 @@
 // include Fake lib
 #r "packages/FAKE/tools/FakeLib.dll"
 open Fake
+<<<<<<< HEAD
+=======
+open Fake.Testing
+>>>>>>> dev
 
 RestorePackages()
 
 // Properties
 let buildDirs = [ "AzureDocumentDbTypeProvider/bin"; "AzureDocumentDbTypeProvider.Tests/bin" ]
+<<<<<<< HEAD
+=======
+let testDir = "./.test/"
+>>>>>>> dev
 
 // Targets
 Target "Clean" (fun _ ->
     trace "-----Clean previous build-----"
     CleanDirs buildDirs
+<<<<<<< HEAD
+=======
+    CleanDir testDir
+>>>>>>> dev
 )
 
 Target "Default" (fun _ ->
    trace "-----Building DEFAULT-----"
 )
 
+<<<<<<< HEAD
 Target "BuildDebug" (fun _ ->
     trace "-----Build using DEBUG configuration-----"
     !!("*.sln")
@@ -27,6 +40,36 @@ Target "BuildDebug" (fun _ ->
 "Clean"
   ==> "BuildDebug"
   ==> "Default"
+=======
+Target "BuildTestProj"(fun _ ->
+    trace "-----Build Test Project-----"
+    !!("AzureDocumentDbTypeProvider.Tests\AzureDocumentDbTypeProvider.Tests.sln")
+    |> MSBuildDebug testDir "Build"
+    |> Log "AppBuild-Output: "
+    )
+
+Target "BuildDebug" (fun _ ->
+    trace "-----Build using DEBUG configuration-----"
+    !!("AzureDocumentDbTypeProvider.sln")
+    |> MSBuildDebug "" "Build"
+    |> Log "AppBuild-Output: ")
+
+Target "Test" (fun _ ->
+    trace "Running Tests"
+    let testDlls = !! (testDir @@ "*Tests.dll")
+    testDlls |> Seq.iter (fun i -> trace i)
+    testDlls |> xUnit id
+)
+
+// Dependencies
+"Clean"
+  ==> "BuildDebug"
+
+"BuildDebug"
+    ==> "BuildTestProj"
+    ==> "Test"
+    ==> "Default"
+>>>>>>> dev
 
 // start build
 RunTargetOrDefault "Default"
