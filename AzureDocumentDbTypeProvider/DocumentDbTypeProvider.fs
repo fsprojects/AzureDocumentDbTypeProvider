d946d69a46cff4dfaf181da38ad022d1814cc422
Stewart Robertson <stewart_robertson@sky.com>
2016-07-14 08:34:18 +0100

    wip

---
+++ AzureDocumentDbTypeProvider/DocumentDbTypeProvider.fs
@@ -0,0 +1,19 @@
+﻿namespace DocumentDbTypeProvider
+
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+open System.Reflection
+
+[<TypeProvider>]
+type public DocumentDbTypeProvider(config: TypeProviderConfig) as this = 
+    inherit TypeProviderForNamespaces()
+
+    let nsName = "FSharp.Azure.DocumentDbTypeProvider"
+    let thisAssembly = Assembly.GetExecutingAssembly()
+    let docDbType = ProvidedTypeDefinition(thisAssembly,nsName,"DocumentDbTypeProvider", baseType = Some typeof<obj>)
+
+    do
+        this.AddNamespace(nsName,[docDbType])
+
+[<TypeProviderAssembly>]
+do ()
