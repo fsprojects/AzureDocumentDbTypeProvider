5b63061324e27faaa8df719247d28eafc1a7428c
Stewart Robertson <stewart_robertson@sky.com>
2016-08-14 10:29:05 +0100

    Fixes #5 (#14)
    * auto generate docs
    
    * fixes #5

--- build.fsx
+++ build.fsx
@@ -4,6 +4,7 @@
 
 open Fake
 open Fake.Testing
+open Fake.Git
 open Microsoft.Azure.Documents.Client
 open Microsoft.Azure.Documents
 open System
@@ -16,6 +17,7 @@ let description = "The DocumentDb Type Provider provides easy access to database
 let releaseNotes = "This package is still in development"
 let deploymentsDir = "./.deploy/"
 let buildDir = "./.build/"
+let binDir = "./bin/"
 let testAcUri = environVar "test_acc_uri"
 let testAcKey = environVar "test_acc_key"
 
@@ -33,17 +35,28 @@ let packageDir = "./.package/"
 let buildDirs = [ "AzureDocumentDbTypeProvider/bin"; "AzureDocumentDbTypeProvider.Tests/bin"; buildDir ]
 let testDir = "./.test/"
 
+// Git configuration (used for publishing documentation in gh-pages branch)
+// The profile where the project is posted
+let gitOwner = "stewart-r"
+let gitHome = "https://github.com/" + gitOwner
+
+// The name of the project on GitHub
+let gitName = "AzureDocumentDbTypeProvider"
+
+// The url for the raw files hosted
+let gitRaw = environVarOrDefault "gitRaw" "https://raw.github.com/stewart-r"
+
 
 //Sets up directories used in build process
 Target "SetUp" (fun _ -> 
-        [buildDir;packageDir;testDir;deploymentsDir] |> Seq.iter(fun d -> ensureDirectory d)
+        [buildDir;packageDir;testDir;deploymentsDir;binDir] |> Seq.iter(fun d -> ensureDirectory d)
     )   
 
 // Targets
 Target "Clean" (fun _ ->
     trace "-----Clean previous build-----"
     CleanDirs buildDirs
-    CleanDirs [testDir;packageDir]
+    CleanDirs [testDir;packageDir;binDir]
 )
 
 Target "BuildTestProj"(fun _ ->
@@ -169,6 +182,95 @@ let deleteCollection dbId name =
     |> Async.RunSynchronously
     |> ignore
 
+let fakePath = "packages" </> "FAKE" </> "tools" </> "FAKE.exe"
+let fakeStartInfo script workingDirectory args fsiargs environmentVars =
+    (fun (info: System.Diagnostics.ProcessStartInfo) ->
+        info.FileName <- System.IO.Path.GetFullPath fakePath
+        info.Arguments <- sprintf "%s --fsiargs -d:FAKE %s \"%s\"" args fsiargs script
+        info.WorkingDirectory <- workingDirectory
+        let setVar k v =
+            info.EnvironmentVariables.[k] <- v
+        for (k, v) in environmentVars do
+            setVar k v
+        setVar "MSBuild" msBuildExe
+        setVar "GIT" Git.CommandHelper.gitPath
+        setVar "FSI" fsiPath)
+
+/// Run the given buildscript with FAKE.exe
+let executeFAKEWithOutput workingDirectory script fsiargs envArgs =
+    let exitCode =
+        ExecProcessWithLambdas
+            (fakeStartInfo script workingDirectory "" fsiargs envArgs)
+            TimeSpan.MaxValue false ignore ignore
+    System.Threading.Thread.Sleep 1000
+    exitCode
+
+// Documentation
+let buildDocumentationTarget fsiargs target =
+    trace (sprintf "Building documentation (%s), this could take some time, please wait..." target)
+    let exit = executeFAKEWithOutput "docs/tools" "generate.fsx" fsiargs ["target", target]
+    if exit <> 0 then
+        failwith "generating reference documentation failed"
+    ()
+
+Target "GenerateReferenceDocs" (fun _ ->
+    buildDocumentationTarget "-d:RELEASE -d:REFERENCE" "Default"
+)
+
+let generateHelp' fail debug =
+    let args =
+        if debug then "--define:HELP"
+        else "--define:RELEASE --define:HELP"
+    try
+        buildDocumentationTarget args "Default"
+        traceImportant "Help generated"
+    with
+    | e when not fail ->
+        traceImportant "generating help documentation failed"
+
+let generateHelp fail =
+    generateHelp' fail false
+
+Target "GenerateHelp" (fun _ ->
+    DeleteFile "docs/content/release-notes.md"
+    CopyFile "docs/content/" "RELEASE_NOTES.md"
+    Rename "docs/content/release-notes.md" "docs/content/RELEASE_NOTES.md"
+
+    DeleteFile "docs/content/license.md"
+    CopyFile "docs/content/" "LICENSE.txt"
+    Rename "docs/content/license.md" "docs/content/LICENSE.txt"
+
+    generateHelp true
+)
+
+Target "GenerateHelpDebug" (fun _ ->
+    DeleteFile "docs/content/release-notes.md"
+    CopyFile "docs/content/" "RELEASE_NOTES.md"
+    Rename "docs/content/release-notes.md" "docs/content/RELEASE_NOTES.md"
+
+    DeleteFile "docs/content/license.md"
+    CopyFile "docs/content/" "LICENSE.txt"
+    Rename "docs/content/license.md" "docs/content/LICENSE.txt"
+
+    generateHelp' true true
+)
+
+Target "KeepRunning" (fun _ ->
+    use watcher = !! "docs/content/**/*.*" |> WatchChanges (fun changes ->
+         generateHelp' true true
+    )
+
+    traceImportant "Waiting for help edits. Press any key to stop."
+
+    System.Console.ReadKey() |> ignore
+
+    watcher.Dispose()
+)
+
+Target "CopyBin" (fun _ -> 
+    Copy binDir (!!(packageDir + "**"))
+)
+
 FinalTarget "CleanTestData" (fun _ ->
     deleteDb "test1"
     deleteDb "test2"
@@ -183,7 +285,50 @@ Target "InitTestData" (fun _ ->
     createCollection "test1" "TestCollection"
     )
 
+Target "CleanDocs" (fun _ ->
+    CleanDirs ["docs/output"]
+)
+
+Target "GenerateDocs" DoNothing
+
+Target "ReleaseDocs" (fun _ ->
+    let tempDocsDir = "temp" </> "gh-pages"
+    ensureDirectory tempDocsDir
+    CleanDir tempDocsDir
+    Repository.cloneSingleBranch "" (gitHome + "/" + gitName + ".git") "gh-pages" tempDocsDir
+
+    Git.CommandHelper.runSimpleGitCommand tempDocsDir "rm . -f -r" |> ignore
+    CopyRecursive "docs/output" tempDocsDir true |> tracefn "%A"
+    
+    StageAll tempDocsDir
+    Git.Commit.Commit tempDocsDir "Update generated documentation"
+    Branches.push tempDocsDir)
+
 "InitTestData" ==> "BuildTestProj"
+
+"GenerateDocs" ==> "ReleaseDocs"
+
+"CopyBin"
+  ==> "GenerateHelp"
+  ==> "GenerateReferenceDocs"
+  ==> "GenerateDocs"
+
+"CopyBin"
+  ==> "GenerateHelpDebug"
+
+"CleanDocs"
+  ==> "GenerateHelp"
+  ==> "GenerateReferenceDocs"
+  ==> "GenerateDocs"
+
+"CleanDocs"
+  ==> "GenerateHelpDebug"
+
+"BuildPackage"
+  ==> "CopyBin"
+
+"GenerateHelpDebug"
+  ==> "KeepRunning"
 
 "Clean"
   ==> "BuildDebug"
@@ -191,12 +336,10 @@ Target "InitTestData" (fun _ ->
 "Clean"
   ==> "BuildRelease"
 
-"SetUp"
-    ==> "BuildDebug"
-"SetUp"
-    ==> "BuildRelease"
-"SetUp"
-    ==> "BuildTestProj"
+"SetUp" ==> "BuildRelease"
+"SetUp" ==> "BuildDebug"
+"SetUp" ==> "BuildTestProj"
+"SetUp" ==> "CopyBin"
 
 "SetTestAccountCreds" 
     ==> "BuildTestProj"
